586fa6b6250115fd0fd2e861464694a135299650
9CodingAndCoffee <francisco.marchini.85@gmail.com>
2022-08-23 22:53:38 -0300

    feat: create app

---
+++ _next/static/Hf-_09IeK36c-7vjEvEzH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/332-bfa648fb5f016bc0.js","static/chunks/pages/index-0b5980f635fdd41b.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
